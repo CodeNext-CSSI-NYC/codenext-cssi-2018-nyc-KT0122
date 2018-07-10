9dc5e6df25eccad799873ab5fae20ab338121282
Khalil Tavarez <khaliltavarez@gmail.com>
2018-07-10 17:01:30 -0400

    End of day commit

---
+++ coordinate-plane.js
@@ -0,0 +1,28 @@
+// Author: Khalil Tavarez
+
+var readline = require("readline-sync");
+var x1;
+var x2;
+var y1;
+var y2;
+
+x1 = parseInt(readline.question("What is the x1"));
+y1 = parseInt(readline.question("What is the y1"));
+x2 = parseInt(readline.question("What is the x2"));
+y2 = parseInt(readline.question("What is the y2"));
+
+const userFormula =  parseInt(readline.question("Would you like to find the slope of the line(1), the lenght of the line(2), or the midpoint of the line(3)?"));
+
+//Slope: m = (y2 - y1)/(x2 - x1)
+//Midpoint:
+//Distance: d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+if(userFormula == 1){
+
+}
+else if(userFormula == 2){
+
+}
+else {
+
+}
---
+++ shortcuts-practice.js
@@ -0,0 +1,25 @@
+// 1. Decrease the variable below by 1 using two different shortcuts.
+// Print out its value after each step to make sure it worked.
+let a = 7;
+
+let f = a--;
+console.log(f);
+console.log(a);
+
+// 2. Double the value of the variable below using a multiplication shortcut.
+// Print out its value before and after each step to make sure it worked.
+let b = 18;
+console.log(b *= 2);
+// Bonus: Can you think of a way to double the value of a variable using an addition shortcut? Hmm.... (Note: you would never do this, since readability of code is very important and this would be confusing.)
+
+// 3. Predict the value of d. Then print it out to see if you were right.
+let c = 13;
+let d = c++;
+//14
+console.log(c);
+
+// 4. Move the decimal two spots to the left using a shortcut.
+// Print out the value of e before and after to make sure it worked.
+let e = 234.5;
+console.log(e);
+console.log(e /= 100);
